de89c66c60164fc7d6efcfffda37a44e8b600790
Teknokrata Indonesia <aldii.perdanaa@gmail.com>
2024-06-16 19:59:47 +0800

    Detection

--- app/src/main/java/com/capstone/berrets/view/main/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/berrets/view/main/home/HomeFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.capstone.berrets.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
